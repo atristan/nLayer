130b944b282550d17b1d466ab6bd7a4a5e9ec0b3
atristan <anthony.tristan@contractor.swri.org>
2023-05-05 15:19:53 -0500

    Add project files.

---
+++ Applications/Spaanjaars.ContactManager45.Web.WebForms/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-3.6.0.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="modernizr-2.0.6-development-only.js" />
